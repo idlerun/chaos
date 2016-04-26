ff4e30b59e7e09b3e2433cf6f50a1c698ceab06e
Jeff Crowe <jeff@jeffjcrowe.com>
2016-04-25 19:47:02 -0700

    Initial Commit

---
+++ Chaos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chaos
+//
+//  Created by Jeff Crowe on 2016-04-25.
+//  Copyright © 2016 idlerun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
